15c8b90cfd2d25784d14bb6e0f4217156acf1b21
Benjamin Garcia <bengarc@gmail.com>
2025-11-21 09:27:32 +0100

    feat(navbar): use useLocation to keep user on same page when clicking logo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,84 +1,71 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { useLocation, Link } from 'react-router-dom'
 import { Popover, PopoverButton, PopoverPanel } from '@headlessui/react'
 
 export default function Navbar(): React.ReactElement {
+  const location = useLocation()
+  console.log(location.pathname)
   return (
     <nav className=" p-4 flex justify-between items-center fixed top-0 left-0 right-0 bg-white z-10">
       <div className="">
-        <a href="/">
+        <button
+          onClick={() => window.location.reload()}
+          className="cursor-pointer"
+        >
           <h1 className="font-sans font-normal text-[34px] uppercase tracking-widest">
             Benjamin Garcia
           </h1>
-        </a>
+        </button>
       </div>
       <ul className="flex space-x-4">
         <li>
           <Popover className="relative z-11">
-            <PopoverButton className="focus:outline-none hover:text-gray-400 font-light">
-              Paintings
-            </PopoverButton>
-            <PopoverPanel
-              anchor="bottom"
-              // className="flex flex-col justify-center items-center py-4 bg-amber-700 w-40 "
-              className="absolute left-0 mt-2 w-48 shadow-lg  ring-opacity-5 z-20 bg-gray-800 p-4"
-            >
-              <div className="flex flex-col w-full gap-4 ">
-                {[
-                  '2025',
-                  '2024',
-                  '2023',
-                  '2022',
-                  '2021',
-                  '2020',
-                  '2018',
-                  '2017',
-                  '2016',
-                  '2015',
-                  '2014',
-                  '2013',
-                ].map((item) => (
-                  <a
-                    key={item}
-                    className="text-white"
-                    href={`/paintings/${item.toLowerCase()}`}
-                  >
-                    {item}
-                  </a>
-                ))}
-              </div>
-            </PopoverPanel>
+            {({ close }) => (
+              <>
+                <PopoverButton className="focus:outline-none hover:text-gray-400 font-light cursor-pointer">
+                  Paintings
+                </PopoverButton>
+                <PopoverPanel
+                  anchor="bottom"
+                  className="absolute left-0 mt-2 w-48 shadow-lg ring-opacity-5 z-20 bg-gray-800 p-4"
+                >
+                  <div className="flex flex-col w-full gap-4">
+                    {[
+                      '2025',
+                      '2024',
+                      '2023',
+                      '2022',
+                      '2021',
+                      '2020',
+                      '2018',
+                      '2017',
+                      '2016',
+                      '2015',
+                      '2014',
+                      '2013',
+                    ].map((item) => (
+                      <Link
+                        key={item}
+                        className="text-white hover:text-gray-400"
+                        to={`/paintings/${item.toLowerCase()}`}
+                        onClick={() => close()} // <-- call close here
+                      >
+                        {item}
+                      </Link>
+                    ))}
+                  </div>
+                </PopoverPanel>
+              </>
+            )}
           </Popover>
         </li>
-        {/* <li>
-          <a href="/archive" className=" hover:text-gray-400 font-light">
-            Archive
-          </a>
-        </li> */}
-        {/* <li>
-          <a href="drawings" className=" hover:text-gray-400 font-light">
-            Drawings
-          </a>
-        </li> */}
-        <li>
-          <a href="/store" className=" hover:text-gray-400 font-light">
-            Store
-          </a>
-        </li>
-        <li>
-          <a href="/about" className=" hover:text-gray-400 font-light">
-            About
-          </a>
-        </li>
-        <li>
-          <a href="/newsletter" className=" hover:text-gray-400 font-light">
-            Newsletter
-          </a>
-        </li>
-        <li>
-          <a href="/contact" className=" hover:text-gray-400 font-light">
-            Contact
-          </a>
-        </li>
+        {['about', 'newsletter', 'contact'].map((item, key) => (
+          <li key={key}>
+            <Link to={`/${item}`} className="hover:text-gray-400 font-light">
+              {item.charAt(0).toUpperCase() + item.slice(1)}
+            </Link>
+          </li>
+        ))}
       </ul>
     </nav>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,14 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ArchivePage from './pages/ArchivePage'
 import YearPage from './pages/YearPage'
 import Navbar from './components/Navbar'
+import About from './pages/About'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <Navbar />
     <BrowserRouter>
+      <Navbar />
       <Routes>
+        <Route path="/about" element={<About />} />
         <Route path="/archive" element={<ArchivePage />} />
         <Route path="/paintings/:year" element={<YearPage />} />
       </Routes>
