7af67c989e3d799d2dbec96836b9281215e15f5c
Benjamin Garcia <bengarc@gmail.com>
2025-12-02 15:35:19 +0100

    chore(app): update index and main entry; add favicon locally

--- src/main.tsx
+++ src/main.tsx
@@ -1,32 +1,13 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App'
-import ArchivePage from './pages/ArchivePage'
 import YearPage from './pages/YearPage'
-import Navbar from './components/Navbar'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Newsletter from './pages/Newsletter'
 import Landing from './pages/Landing'
+import './index.css'
 
-// createRoot(document.getElementById('root')!).render(
-//   <StrictMode>
-//     <BrowserRouter>
-//       <Navbar />
-//       <Routes>
-//         <Route path="/" element={<Landing />} />
-//         <Route path="/paintings/:year" element={<YearPage />} />
-//         <Route path="/about" element={<About />} />
-//         <Route path="/newsletter" element={<Newsletter />} />
-//         <Route path="/contact" element={<Contact />} />
-//         <Route path="/archive" element={<ArchivePage />} />
-//       </Routes>
-//     </BrowserRouter>
-//   </StrictMode>
-// )
 const router = createBrowserRouter([
   {
     path: '/',
